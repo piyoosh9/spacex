e73e8d872e4736ef4d11e69c152431fdc8ef5270
Piyush Modi <piyushmodi9@gmail.com>
2021-02-21 10:28:13 +0530

    server side rendering added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { SpaceXCommonModule } from './views/modules/common/common.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
     SpaceXCommonModule
