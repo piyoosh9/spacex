90b070c245f83045cd6545eab8bc175bb9f1d2d9
Piyush Modi <piyushmodi9@gmail.com>
2021-02-21 16:22:21 +0530

    added compression

--- server.ts
+++ server.ts
@@ -7,10 +7,11 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
-
+var compression = require('compression');
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
+  server.use(compression());
   const distFolder = join(process.cwd(), 'dist/spacex/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
